c0ef815a6b664a8a4d99db0d2630b0e6e61dc5a2
Eric Siqueira <eric.siqueira005@gmail.com>
2025-03-06 13:22:24 +0530

    Add files via upload

--- JavaLabExp5/Circle.java
+++ JavaLabExp5/Circle.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 // Circle class extends Shape
-class Circle extends Shape {
+public class Circle extends Shape {
     private final double radius;
 
     // Constructor
@@ -27,40 +27,6 @@ class Circle extends Shape {
         System.out.print("Enter the radius of the circle: ");
         double radius = sc.nextDouble();
         return new Circle(radius);
-    }
-}
-
-// Rectangle class extends Shape
-class Rectangle extends Shape {
-    private final double length;
-    private final double width;
-    
-    // Constructor
-    public Rectangle(double length, double width) {
-        super("Rectangle");
-        this.length = length;
-        this.width = width;
-    }
-
-    // Override abstract methods
-    @Override
-    public double calculateArea() {
-        return length * width;
-    }
-
-    @Override
-    public double calculatePerimeter() {
-        return 2 * (length + width);
-    }
-
-    // Static method to create a Rectangle from user input
-    public static Rectangle createFromUserInput() {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the length of the rectangle: ");
-        double length = sc.nextDouble();
-        System.out.print("Enter the width of the rectangle: ");
-        double width = sc.nextDouble();
-        return new Rectangle(length, width);
     }
 }
 
