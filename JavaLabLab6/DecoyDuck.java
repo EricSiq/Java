51a2e9fb575e5330d1ca375976f46587cd4898ae
Eric Siqueira <eric.siqueira005@gmail.com>
2025-03-06 09:58:56 +0530

    Update DecoyDuck.java

--- JavaLabLab6/DecoyDuck.java
+++ JavaLabLab6/DecoyDuck.java
@@ -1,15 +1,15 @@
 // DecoyDuck - A type of Duck that does not fly, does not quack, and drowns
 public class DecoyDuck extends Duck {
-    
+
     /**
      * Constructor for DecoyDuck.
      * Uses composition to set behavior dynamically.
      */
     public DecoyDuck() {
         super("Decoy Duck", 
-              new NoFly(),          // Cannot fly
+              new NoFLy(),          // Cannot fly
               new MuteQuack(),      // Cannot quack
-              new SwimBehaviorImpl(SwimBehaviorImpl.SwimType.DROWN)); // Drowns instead of swimming
+              new Swim(Swim.SwimType.DROWN)); // Drowns instead of swimming
     }
 
     /**
