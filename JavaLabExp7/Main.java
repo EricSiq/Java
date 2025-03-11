9420af8addc451c279f0ba2516b11aa3ecebe1e0
Eric Siqueira <eric.siqueira005@gmail.com>
2025-03-11 21:56:36 +0530

    Create Main.java
    11.3.25 labwork

---
+++ JavaLabExp7/Main.java
@@ -0,0 +1,6 @@
+public class Main{
+    public static void main(String[] args){
+        StudentOperations StudentOperations = new StudentOperations();
+        StudentOperations.insertStudent();
+    }
+}
