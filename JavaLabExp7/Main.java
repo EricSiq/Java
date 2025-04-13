7143b88aa1f528320c74066d8064ac5bcb43fdb2
Eric Siqueira <eric.siqueira005@gmail.com>
2025-04-13 19:51:34 +0530

    Update and rename Main.java to CalculatorMain.java

--- JavaLabExp7/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main{
-    public static void main(String[] args){
-        StudentOperations StudentOperations = new StudentOperations();
-        StudentOperations.insertStudent();
-    }
-}
