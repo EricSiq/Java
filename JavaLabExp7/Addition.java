f03817ff3b9ebdd124e3ab39c8273b7f73340338
Eric Siqueira <eric.siqueira005@gmail.com>
2025-04-13 19:53:18 +0530

    Create Addition.java

---
+++ JavaLabExp7/Addition.java
@@ -0,0 +1,14 @@
+// Class to perform addition operation.
+public class Addition {
+    
+    /**
+     * Adds two numbers.
+     *
+     * @param a First number.
+     * @param b Second number.
+     * @return Sum of a and b.
+     */
+    public static double add(double a, double b) {
+        return a + b;
+    }
+}
