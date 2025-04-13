c4c23649a38e6352c30587f95c51729e18d6c958
Eric Siqueira <eric.siqueira005@gmail.com>
2025-04-13 19:55:06 +0530

    Create Cube.java

---
+++ JavaLabExp7/Cube.java
@@ -0,0 +1,13 @@
+// Class to calculate the cube of a number.
+public class Cube {
+    
+    /**
+     * Returns the cube of the given number.
+     *
+     * @param a The number to cube.
+     * @return Cube of a.
+     */
+    public static double cube(double a) {
+        return a * a * a;
+    }
+}
