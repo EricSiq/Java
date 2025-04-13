f2ed4160394ad2d2553abed30c1b610fdac1cba8
Eric Siqueira <eric.siqueira005@gmail.com>
2025-04-13 19:53:55 +0530

    Create Multiplication.java

---
+++ JavaLabExp7/Multiplication.java
@@ -0,0 +1,14 @@
+// Class to perform multiplication operation.
+public class Multiplication {
+    
+    /**
+     * Multiplies two numbers.
+     *
+     * @param a First number.
+     * @param b Second number.
+     * @return Product of a and b.
+     */
+    public static double multiply(double a, double b) {
+        return a * b;
+    }
+}
