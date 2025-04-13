71ae78b55ca8cb0eecdeca32cc3ba343f50d997b
Eric Siqueira <eric.siqueira005@gmail.com>
2025-04-13 19:53:35 +0530

    Create Subtraction.java

---
+++ JavaLabExp7/Subtraction.java
@@ -0,0 +1,14 @@
+// Class to perform subtraction operation.
+public class Subtraction {
+    
+    /**
+     * Subtracts second number from first.
+     *
+     * @param a First number.
+     * @param b Second number.
+     * @return Difference of a and b.
+     */
+    public static double subtract(double a, double b) {
+        return a - b;
+    }
+}
