59bea34c0c571a1208af2d7b387a8fc57adc0fe7
Eric Siqueira <eric.siqueira005@gmail.com>
2025-04-13 19:54:38 +0530

    Create Square.java

---
+++ JavaLabExp7/Square.java
@@ -0,0 +1,13 @@
+// Class to calculate the square of a number.
+public class Square {
+    
+    /**
+     * Returns the square of the given number.
+     *
+     * @param a The number to square.
+     * @return Square of a.
+     */
+    public static double square(double a) {
+        return a * a;
+    }
+}
