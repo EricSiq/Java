6e4699e55796ccf7731c8a076d20c1561b4c2671
Eric Siqueira <eric.siqueira005@gmail.com>
2025-04-14 22:56:55 +0530

    Create DuplicateStudentException.java

---
+++ JavaLabExp8/DuplicateStudentException.java
@@ -0,0 +1,7 @@
+// Thrown when attempting to add a student with an existing ID.
+
+public class DuplicateStudentException extends Exception {
+    public DuplicateStudentException(String message) {
+        super(message);
+    }
+}
